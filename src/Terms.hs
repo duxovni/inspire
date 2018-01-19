d49c5adb5b2e5e89dd92a93ba98b5bcd7d068ea0
ikdc <ikdc@mit.edu>
2018-01-18 20:43:40 -0500

    fix substitution through matches

--- src/Terms.hs
+++ src/Terms.hs
@@ -34,7 +34,10 @@ data Expr = EVar Int
           | EInt Int
           | EString String
           | EBinOp BinOp Expr Expr
-          | EMatch IndName Expr (Map.Map ConstrName Expr)
+          -- IndName is the name of the inductive type of the match
+          -- Each branch contains the branch body and the number of
+          -- bound variables
+          | EMatch IndName Expr (Map.Map ConstrName (Int, Expr))
   deriving (Show, Eq)
 
 subst :: Int -> Expr -> Expr -> Expr
@@ -46,7 +49,9 @@ subst d x (EConstr n l) = EConstr n (fmap (subst d x) l)
 subst d x (EAbs b) = EAbs (subst (d + 1) x b)
 subst d x (EApp f y) = EApp (subst d x f) (subst d x y)
 subst d x (EBinOp b e1 e2) = EBinOp b (subst d x e1) (subst d x e2)
-subst d x (EMatch i e m) = EMatch i (subst d x e) (fmap (subst d x) m)
+subst d x (EMatch i e m) =
+  EMatch i (subst d x e)
+  (fmap (\(k, e) -> (k, subst (d + k) x e)) m)
 subst d x e = e
 
 multisubst :: Int -> [Expr] -> Expr -> Expr
@@ -54,7 +59,7 @@ multisubst d [] b = b
 multisubst d (x:xs) b = multisubst (d - 1) xs (subst d x b)
 
 eval :: EDefs -> Expr -> Expr
-eval c (EVar v) = error ("Free variable " ++ show v ++ " evaluated")
+eval c (EVar v) = error $ "Free variable " ++ show v ++ " evaluated"
 eval c (EName n) = eval c (c Map.! n)
 eval c (EApp f a) =
   case eval c f of
@@ -68,6 +73,6 @@ eval c (EBinOp b e1 e2) =
         _ -> error "int does not reduce to constant"
 eval c (EMatch _ x m) =
   case eval c x of
-    EConstr n l -> eval c (multisubst (length l - 1) l (m Map.! n))
+    EConstr n l -> eval c $ multisubst (length l - 1) l (snd $ m Map.! n)
     _ -> error "matched term does not reduce to constr"
 eval c x = x
--- test/TermsSpec.hs
+++ test/TermsSpec.hs
@@ -4,6 +4,13 @@ import Test.Hspec
 import Terms
 import qualified Data.Map as Map
 
+epred :: Expr
+epred = EAbs (EMatch "nat"
+              (EVar 0)
+              (Map.fromList
+                [("O", (0, EConstr "O" [])),
+                 ("S", (1, EVar 0))]))
+
 spec :: Spec
 spec = do
   describe "eval" $ do
@@ -20,5 +27,20 @@ spec = do
       (EMatch
         "whatever"
         (EApp (EAbs (EConstr "c1" [(EInt 5), (EInt 6)])) (EInt 7))
-        (Map.fromList [("c0", EVar 1), ("c1", EVar 0)]))
+        (Map.fromList [("c0", (2, EVar 1)), ("c1", (2, EVar 0))]))
       `shouldBe` EInt 6
+
+    it "evaluates pred 0 correctly" $
+      eval (Map.fromList [("pred", epred)])
+      (EApp (EName "pred") (EConstr "O" []))
+      `shouldBe` EConstr "O" []
+
+    it "evaluates pred 1 correctly" $
+      eval (Map.fromList [("pred", epred)])
+      (EApp (EName "pred") (EConstr "S" [EConstr "O" []]))
+      `shouldBe` EConstr "O" []
+
+    it "evaluates pred 2 correctly" $
+      eval (Map.fromList [("pred", epred)])
+      (EApp (EName "pred") (EConstr "S" [EConstr "S" [EConstr "O" []]]))
+      `shouldBe` EConstr "S" [EConstr "O" []]
